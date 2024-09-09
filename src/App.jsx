4f33103dbc2e3fc926dc3b72c37db3169865090a
Syed Shahwaiz Qasim <syedshahwaiz32@gmail.com>
2024-09-09 12:01:59 -0700

    styling complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,43 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [Amount, setAmount] = useState(0);
+  const [Type, setType] = useState("Income");
+  const [Transaction, setTransaction] = useState([]);
+
+    console.log("Amount=>", Amount);
+    console.log("Type=>", Type);
+    setTransaction([...Transaction, { Amount, Type }]);
+  console.log("Transaction", Transaction);
 
   return (
     <>
-     <div className='container'>
-      <form>
-        <h2>Finance Manangement</h2>
-        <input type="number" placeholder='Add Amount' className='myInput' />
-        <select className='myInput'>
-          <option value="income">Income</option>
-          <option value="expensive">Expensive</option>
-        </select>
-      </form>
-     </div>
+      <div className="container">
+        <form>
+          <h2>Finance Manangement</h2>
+          <input
+            type="number"
+            value={Amount}
+            onChange={(e) => setAmount(e.target.value)}
+            placeholder="Add Amount"
+            className="myInput"
+          />
+          <select
+            className="myInput"
+            value={Type}
+            onChange={(e) => setType(e.target.value)}
+          >
+            <option value="Income">Income</option>
+            <option value="Expensive">Expensive</option>
+          </select>
+          <button className="btn1" onClick={handleOnButton}>
+            Submit
+          </button>
+        </form>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
