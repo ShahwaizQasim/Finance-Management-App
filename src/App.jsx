3e850650debb9043df95165f98206ecf4af05039
Syed Shahwaiz Qasim <syedshahwaiz32@gmail.com>
2024-09-09 19:54:11 -0700

    js add

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,31 @@
 import { useState } from "react";
+import './index.css'
 import "./App.css";
 
 function App() {
+  // const [Amount, setAmount] = useState(0);
+  // const [Type, setType] = useState("Income");
+  // const [Transaction, setTransaction] = useState([]);
+
+  //   console.log("Amount=>", Amount);
+  //   console.log("Type=>", Type);
   const [Amount, setAmount] = useState(0);
   const [Type, setType] = useState("Income");
   const [Transaction, setTransaction] = useState([]);
 
+  const handleOnButton = () => {
     console.log("Amount=>", Amount);
     console.log("Type=>", Type);
     setTransaction([...Transaction, { Amount, Type }]);
+    setAmount("");
+    setType("Income");
+  };
   console.log("Transaction", Transaction);
 
   return (
     <>
       <div className="container">
-        <form>
+
           <h2>Finance Manangement</h2>
           <input
             type="number"
@@ -31,10 +42,23 @@ function App() {
             <option value="Income">Income</option>
             <option value="Expensive">Expensive</option>
           </select>
-          <button className="btn1" onClick={handleOnButton}>
+          <div>
+            {
+            Transaction.map((data,index) => {
+              return (
+                <div className="userData">
+                  <span></span>
+                  <h3>{index+1}{")"} {data.Amount}</h3>
+                  <h3>{data.Type}</h3>
+                </div>
+                
+              )
+            })
+            }
+          </div>
+          <button  onClick={handleOnButton} className="btn1">
             Submit
           </button>
-        </form>
       </div>
     </>
   );
